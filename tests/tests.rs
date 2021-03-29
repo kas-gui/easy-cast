bd805320dd39695cc0fa9f2756bec70f316ddfcc
Diggory Hardy <git@dhardy.name>
2021-03-29 08:50:38 +0100

    Add float trunc tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -10,7 +10,7 @@ fn int_casts() {
 }
 
 #[test]
-#[cfg(feature = "std")]
+#[cfg(any(feature = "std", feature = "libm"))]
 fn float_casts() {
     assert_eq!(u64::conv_nearest(13.2f32), 13);
     let x: i128 = 13.5f32.cast_nearest();
@@ -18,6 +18,22 @@ fn float_casts() {
     assert_eq!(u8::conv_floor(13.8f64), 13);
     assert_eq!(u32::conv_ceil(13.1f32), 14);
     assert_eq!(i64::conv_floor(-3168565.13), -3168566);
+}
+
+#[test]
+#[cfg(any(feature = "std", feature = "libm"))]
+fn float_trunc() {
+    let xx = [-32768.0f32, -32768.99, -0.99, 0.99, 32767.99];
+    let yy = [-32768i16, -32768, 0, 0, 32767];
+    for (x, y) in xx[..].iter().zip(yy[..].iter()) {
+        assert_eq!(i16::conv_trunc(*x), *y);
+    }
+}
+
+#[test]
+#[should_panic]
+fn float_trunc_fail1() {
+    i16::conv_trunc(32768.0f32);
 }
 
 #[test]
