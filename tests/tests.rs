790a9ba80753b0f985f0265905cd64962d7d45e9
Diggory Hardy <git@dhardy.name>
2022-10-14 12:24:33 +0100

    Extra tests for f32 <-> u128 conversions

--- tests/tests.rs
+++ tests/tests.rs
@@ -49,6 +49,25 @@ fn int_to_float_inexact() {
 }
 
 #[test]
+fn f32_max_to_u128() {
+    let v = 0xFFFFFF00_00000000_00000000_00000000u128;
+    assert_eq!(u128::conv_trunc(f32::MAX), v);
+    assert_eq!(u128::conv_nearest(f32::MAX), v);
+    assert_eq!(u128::conv_floor(f32::MAX), v);
+    assert_eq!(u128::conv_ceil(f32::MAX), v);
+    assert_eq!(u128::conv_approx(f32::MAX), v);
+}
+
+#[test]
+#[should_panic(
+    expected = "cast x: u128 to f32: inexact for x = 340282346638528859811704183484516925441"
+)]
+fn u128_large_to_f32() {
+    let v = 0xFFFFFF00_00000000_00000000_00000000u128 + 1;
+    f32::conv(v);
+}
+
+#[test]
 #[cfg(any(feature = "std", feature = "libm"))]
 fn approx_float_to_int() {
     assert_eq!(i32::conv_approx(1.99f32), 1);
