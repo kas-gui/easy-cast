8b6d83935dd8fab710d9a59ed96cbfd37459e48f
Diggory Hardy <git@dhardy.name>
2026-07-16 09:54:11 +0100

    Simplify ConvApprox<f64> for f32: use as (#47)

--- tests/tests.rs
+++ tests/tests.rs
@@ -91,16 +91,20 @@ fn approx_f64_f32() {
     assert_eq!(f32::conv_approx(1f64), 1f32);
     assert_eq!(f32::conv_approx(1f64 + E32), 1f32 + f32::EPSILON);
     assert_eq!(f32::conv_approx(1f64 + E32 / 2.0), 1f32);
-    assert_eq!(f32::conv_approx(1f64 + E32 - f64::EPSILON), 1f32);
+    assert_eq!(
+        f32::conv_approx((1f64 + E32 / 2.0).next_up()),
+        1f32.next_up()
+    );
 
     assert_eq!(f32::conv_approx(-10f64), -10f32);
     assert!((f32::conv_approx(1f64 / 3.0) - 1f32 / 3.0).abs() <= f32::EPSILON);
 
     const MAX: f64 = f32::MAX as f64;
     assert_eq!(f32::conv_approx(MAX), f32::MAX);
-    assert!(MAX + 2f64.powi(103) != MAX);
-    assert_eq!(f32::conv_approx(MAX + 2f64.powi(103)), f32::MAX);
-    assert_eq!(f32::try_conv_approx(MAX * 2.0), Err(Error::Range));
+    // last non-zero binary digits of mantissa are 1101, which rounds down:
+    assert_eq!(f32::conv_approx(MAX + 2f64.powi(102)), f32::MAX);
+    // last non-zero binary digits of mantissa are 1110, which rounds up:
+    assert_eq!(f32::conv_approx(MAX + 2f64.powi(103)), f32::INFINITY);
 
     assert_eq!(
         f32::conv_approx(f64::INFINITY).to_bits(),
