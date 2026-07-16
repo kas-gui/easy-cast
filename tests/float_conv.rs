b13779b4ecc9a492b7170cef8fe674eb15299920
Diggory Hardy <git@dhardy.name>
2026-07-16 10:14:46 +0000

    Dedup and organise float tests; remove conv_approx.rs

--- tests/float_conv.rs
+++ tests/float_conv.rs
@@ -3,23 +3,26 @@
 use easy_cast::{Error, traits::*};
 
 #[test]
-#[should_panic(expected = "cast float-to-float: NaN")]
-fn f32_nan_to_f64() {
-    f64::conv(f32::NAN);
-}
-
-#[test]
-#[should_panic(expected = "cast float-to-float: NaN")]
-fn f64_nan_to_f32() {
-    f32::conv_approx(f64::NAN);
-}
-
-#[test]
-fn approx_f64_to_f32() {
+fn f64_to_f32_zero() {
     assert_eq!(f32::conv_approx(0f64), 0f32);
     assert_eq!(f32::conv_approx(0f64).is_sign_positive(), true);
     assert_eq!(f32::conv_approx(-0f64).is_sign_negative(), true);
+}
 
+#[test]
+fn f64_to_f32_subnormal() {
+    assert_eq!(
+        f32::conv_approx((f32::MIN_POSITIVE as f64) / 2.0).to_bits(),
+        1 << 22
+    );
+    assert_eq!(
+        f32::conv_approx((f32::from_bits(1) as f64) / 2.0).to_bits(),
+        0.0f32.to_bits()
+    );
+}
+
+#[test]
+fn f64_to_f32_normal() {
     const E32: f64 = f32::EPSILON as f64;
     assert_eq!(f32::conv_approx(1f64), 1f32);
     assert_eq!(f32::conv_approx(1f64 + E32), 1f32 + f32::EPSILON);
@@ -31,7 +34,10 @@ fn approx_f64_to_f32() {
 
     assert_eq!(f32::conv_approx(-10f64), -10f32);
     assert!((f32::conv_approx(1f64 / 3.0) - 1f32 / 3.0).abs() <= f32::EPSILON);
+}
 
+#[test]
+fn f64_to_f32_overflow() {
     const MAX: f64 = f32::MAX as f64;
     assert_eq!(f32::conv_approx(MAX), f32::MAX);
     // last non-zero binary digits of mantissa are 1101, which rounds down:
@@ -43,5 +49,12 @@ fn approx_f64_to_f32() {
         f32::conv_approx(f64::INFINITY).to_bits(),
         f32::INFINITY.to_bits()
     );
+    assert_eq!(f32::try_conv_approx(f64::MAX), Ok(f32::INFINITY));
+}
+
+#[test]
+fn float_nan() {
+    assert_eq!(f64::try_conv(f32::NAN), Err(Error::Range));
+    assert_eq!(f64::try_conv_approx(f32::NAN), Err(Error::Range));
     assert_eq!(f32::try_conv_approx(f64::NAN), Err(Error::Range));
 }
